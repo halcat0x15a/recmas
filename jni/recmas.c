e93a8e2bf259f2a3445e8aa71084596ef56754c0
sanshiro_yoshida <sanshiro_yoshida@dwango.co.jp>
2017-07-11 19:53:34 +0900

    add command line option

--- jni/recmas.c
+++ jni/recmas.c
@@ -1,4 +1,5 @@
 #include <fcntl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -9,56 +10,83 @@
 
 struct event {
   useconds_t time;
-  struct input_event raw;
+  struct input_event input;
 };
 
-int main(int argc, char *argv[]) {
+useconds_t delta(struct timespec time) {
+  struct timespec now;
+  clock_gettime(CLOCK_MONOTONIC_RAW, &now);
+  return (now.tv_sec - time.tv_sec) * 1000000 + (now.tv_nsec - time.tv_nsec) / 1000;
+}
+
+int record() {
+  int event_fd = open(EVENT, O_RDONLY);
+  bool recording = false;
+  struct timespec start_time;
+  for (;;) {
+    struct input_event input;
+    if (read(event_fd, &input, sizeof(input)) != sizeof(input)) {
+      exit(EXIT_FAILURE);
+    }
+    if (recording) {
+      struct event event;
+      event.time = delta(start_time);
+      event.input = input;
+      write(STDOUT_FILENO, &event, sizeof(event));
+    } else if (input.code == BTN_TOUCH) {
+      clock_gettime(CLOCK_MONOTONIC_RAW, &start_time);
+      recording = true;
+    }
+  }
+}
+
+int play() {
   int event_fd = open(EVENT, O_RDWR);
-  struct timespec rec;
-  int playing = 0;
-  switch (argc) {
-  case 1:
-    for (;;) {
-      struct input_event event;
-      if (read(event_fd, &event, sizeof(event)) != sizeof(event)) {
+  bool playing = false;
+  struct timespec start_time;
+  for (;;) {
+    if (playing) {
+      struct event event;
+      if (read(STDIN_FILENO, &event, sizeof(event)) != sizeof(event)) {
+	exit(EXIT_SUCCESS);
+      }
+      while (delta(start_time) < event.time);
+      write(event_fd, &event.input, sizeof(event.input));
+    } else {
+      struct input_event input;
+      if (read(event_fd, &input, sizeof(input)) != sizeof(input)) {
 	exit(EXIT_FAILURE);
-      } else {
-	if (rec.tv_sec && rec.tv_nsec) {
-	  struct timespec now;
-	  clock_gettime(CLOCK_MONOTONIC_RAW, &now);
-	  useconds_t delta = (now.tv_sec - rec.tv_sec) * 1000000 + (now.tv_nsec - rec.tv_nsec) / 1000;
-	  rec = now;
-	  struct event data;
-	  data.time = delta;
-	  data.raw = event;
-	  write(STDOUT_FILENO, &data, sizeof(data));
-	} else if (BTN_TOUCH == event.code) {
-	  clock_gettime(CLOCK_MONOTONIC_RAW, &rec);
-	}
+      }
+      if (input.code == BTN_TOUCH) {
+	clock_gettime(CLOCK_MONOTONIC_RAW, &start_time);
+	playing = true;
       }
     }
-    return 0;
-  case 2:
-    for (;;) {
-      struct input_event event;
-      if (!playing) {
-	if (read(event_fd, &event, sizeof(event)) != sizeof(event)) {
-	  exit(EXIT_FAILURE);
-	} else {
-	  playing = BTN_TOUCH == event.code;
-	}
-      } else {
-	struct event data;
-	if (read(STDIN_FILENO, &data, sizeof(data)) != sizeof(data)) {
-	  exit(EXIT_SUCCESS);
-	} else {
-	  usleep(data.time);
-	  write(event_fd, &data.raw, sizeof(data.raw));
-	}
-      }
+  }
+}
+
+int print() {
+  for (;;) {
+    struct event event;
+    if (read(STDIN_FILENO, &event, sizeof(event)) != sizeof(event)) {
+      exit(EXIT_SUCCESS);
     }
-    return 0;
-  default:
-    return 1;
+    printf("%llu %d %d %d\n", (uint64_t) event.time, event.input.type, event.input.code, event.input.value);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int opt;
+  while ((opt = getopt(argc, argv, "rwp")) != -1) {
+    switch (opt) {
+    case 'r':
+      return record();
+    case 'w':
+      return play();
+    case 'p':
+      return print();
+    default:
+      exit(EXIT_FAILURE);
+    }
   }
 }
